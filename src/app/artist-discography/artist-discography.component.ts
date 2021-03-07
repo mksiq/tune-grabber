79f6a894f99e653ddc70b5bd0f13deb46fb5d41b
mksiq <maickelsiqueira@gmail.com>
2021-03-06 19:14:44 -0500

    created artist template page

--- src/app/artist-discography/artist-discography.component.ts
+++ src/app/artist-discography/artist-discography.component.ts
@@ -3,24 +3,22 @@ import { Album } from 'src/model/album.model';
 import { Artist } from 'src/model/artist.model';
 import * as albumData from '../data/SearchResultsAlbums.json';
 import * as artistData from '../data/SearchResultsArtist.json';
+import { Util } from '../helper/util';
 
 @Component({
   selector: 'app-artist-discography',
   templateUrl: './artist-discography.component.html',
-  styleUrls: ['./artist-discography.component.css']
+  styleUrls: ['./artist-discography.component.css'],
 })
 export class ArtistDiscographyComponent implements OnInit {
-  // albums: Array<Album>;
-  // artist: Artist;
-  constructor(albums: Array<Album>, artist : Artist ) {
-    // this.albums = albums;
-    // this.artist = artist;
-   }
+  albums?: Array<Album>;
+  artist?: Artist;
+  util?: Util;
+  constructor() {}
 
   ngOnInit(): void {
-    // this.albums = albumData.albums.items;
-    // this.artist = (artistData as any).default;
-  //  console.log(albumData)
+    this.util = new Util();
+    this.artist = (artistData as any).default;
+    this.albums = albumData.albums.items;
   }
-
 }
