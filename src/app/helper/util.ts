31a1d403235b0b01179f3af54316e89f9112e27f
mksiq <maickelsiqueira@gmail.com>
2021-03-06 19:15:41 -0500

    added album page and util class

---
+++ src/app/helper/util.ts
@@ -0,0 +1,21 @@
+export class Util {
+  /**
+   * These functions should be static and the class injected to the components
+   */
+  convertDate(date: string): string {
+    let converted = new Date(date);
+    converted.setDate(converted.getDate() + 1);
+    let dd = converted.getDate();
+    let mm = converted.getMonth() + 1;
+    let yyyy = converted.getFullYear();
+    return `${mm}/${dd}/${yyyy}`;
+  }
+
+  cropTitle(title: string, size: number): string {
+    if (title.length > size) {
+      title = title.substring(0, size - 3);
+      title += '...';
+    }
+    return title;
+  }
+}
