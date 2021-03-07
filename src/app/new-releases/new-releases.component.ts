2f338c01c8bf471f07a5bacc24dfdea5a9336f75
mksiq <maickelsiqueira@gmail.com>
2021-03-06 19:15:17 -0500

    improved main page

--- src/app/new-releases/new-releases.component.ts
+++ src/app/new-releases/new-releases.component.ts
@@ -2,6 +2,7 @@ import { getLocaleDateFormat } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Album } from 'src/model/album.model';
 import * as data from '../data/NewReleasesAlbums.json';
+import { Util } from '../helper/util';
 
 @Component({
   selector: 'app-new-releases',
@@ -10,32 +11,14 @@ import * as data from '../data/NewReleasesAlbums.json';
 })
 export class NewReleasesComponent implements OnInit {
   releases: Array<Album>;
-
+  util?: Util;
   constructor() {
+    this.util = new Util();
     this.releases = [];
   }
 
   ngOnInit(): void {
+    this.util = new Util();
     this.releases = data.albums.items;
-  }
-
-  convertDate(date: string): string {
-    let converted = new Date(date);
-    return (
-      converted.getMonth() +
-      1 +
-      '/' +
-      converted.getDate() +
-      '/' +
-      converted.getFullYear()
-    );
-  }
-
-  cropTitle(title: string, size: number): string {
-    if (title.length > size) {
-      title = title.substring(0, size - 3);
-      title += '...';
-    }
-    return title;
   }
 }
