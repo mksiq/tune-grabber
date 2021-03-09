5f7857fcf5636331149ef6d329cb441422a34eb5
mksiq <maickelsiqueira@gmail.com>
2021-03-09 17:35:14 -0500

    created album card component

---
+++ src/app/album-card/album-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Album } from 'src/model/album.model';
+import { Util } from '../helper/util';
+
+@Component({
+  selector: 'app-album-card',
+  templateUrl: './album-card.component.html',
+  styleUrls: ['./album-card.component.css']
+})
+export class AlbumCardComponent implements OnInit {
+  @Input() album: Album;
+  util: Util;
+  constructor(album: Album) {
+    this.album = album;
+    this.util = new Util();
+   }
+
+  ngOnInit(): void {
+    console.log(this.album);
+    this.util = new Util();
+  }
+}
