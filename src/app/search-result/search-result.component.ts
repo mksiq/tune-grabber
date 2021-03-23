44d52310dc1f9eede18aff03e95aae6c2736f5e6
mksiq <maickelsiqueira@gmail.com>
2021-03-23 11:23:26 -0400

    fixed routes to not refresh page

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Artist } from 'src/model/artist.model';
+
+import { MusicDataService } from '../services/music-data.service';
 
 @Component({
   selector: 'app-search-result',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-result.component.css'],
 })
 export class SearchResultComponent implements OnInit {
-  results: any;
+  results: Array<Artist> = [];
   searchQuery: any;
+  routeSub: any;
+  artistsSub: any;
 
-  constructor() {}
+  constructor( private route: ActivatedRoute,  private musicService: MusicDataService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.routeSub = this.route.params.subscribe((params) => {
+      console.log("called search")
+      this.searchQuery = params.q;
+      this.artistsSub = this.musicService.searchArtists(params.q).subscribe(
+        (data) => {
+          console.log(data)
+          this.results = data.artists?.items;
+        },
+        (error) => console.log(console.log('Handle bad request'))
+      );
+    });
+  }
 }
