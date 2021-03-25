01921e874de0829a555deaca263b83a0ae52afaa
mksiq <maickelsiqueira@gmail.com>
2021-03-25 09:55:14 -0400

    added function to display music length better and source to pallette on css file

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -22,4 +22,15 @@ export class UtilsService {
     }
     return title;
   }
+
+  displayTime(milliseconds: number): string {
+    const totalSeconds = Math.floor(milliseconds / 1000);
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    return (
+      minutes.toString().padStart(2, '0') +
+      ':' +
+      seconds.toString().padStart(2, '0')
+    );
+  }
 }
