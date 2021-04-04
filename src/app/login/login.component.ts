c58fdf02cacd09aba83a01e21fa628c7249d3f31
mksiq <maickelsiqueira@gmail.com>
2021-04-04 13:02:51 -0400

    login view and feature working

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../model/user.model';
+import { AuthService } from '../services/auth-service.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+  user: User = { userName: '', password: '', _id: '' };
+  warning: string = '';
+  loading: boolean = false;
+  authSub: any;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(f: NgForm): void {
+    if (this.user.password && this.user.userName) {      
+      this.loading = true;
+      this.authSub = this.authService.login(this.user).subscribe(
+        (message) => {
+          localStorage.setItem('access_token', message.token);
+          this.loading = false;
+          this.warning = '';
+          this.router.navigate(['/newReleases']);
+        },
+        (error) => {
+          this.loading = false;
+          this.warning = error.error.message;
+        }
+      );
+    } else {
+      this.warning = 'Fill all fields.';
+    }
   }
 
+  ngOnDestroy(): void {
+    this.authSub.unsubscribe();
+  }
 }
